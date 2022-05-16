ed0d0a127456cb7d51cebcb805ff4129829cea8b
Andre Lee <customscriptudg@gmail.com>
2022-05-16 13:34:08 +0800

    Add DestroyOutOfBounds

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float lowerBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < lowerBound)
+            Destroy(gameObject);
+    }
+}
